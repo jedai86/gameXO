841b5019d20afa8ed2be06dda7c525b8105890db
jedai86 <jedai86@yandex.ru>
2016-10-22 21:52:23 +0300

    exceptions created

--- src/ru/jedai86/xo/model/Field.java
+++ src/ru/jedai86/xo/model/Field.java
@@ -6,7 +6,9 @@ import java.awt.*;
  * Created by Jedai86 on 22.10.2016.
  */
 public class Field {
-    private final int FIELD_SIZE = 3;
+    private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
@@ -19,6 +21,14 @@ public class Field {
 
     public void setFigure(final Point point, final Figure figure) {
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 
 }
---
+++ src/ru/jedai86/xo/model/exceptions/InvalidPoinException.java
@@ -0,0 +1,7 @@
+package ru.jedai86.xo.model.exceptions;
+
+/**
+ * Created by Jedai86 on 22.10.2016.
+ */
+public class InvalidPoinException extends XOException {
+}
